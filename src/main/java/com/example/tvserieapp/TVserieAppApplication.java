f9e3f87d352180fc5183496d0ddf2b73fff89de0
mkmbaran <mkmbaran@gmail.com>
2023-06-15 18:34:48 +0200

    Succesfully reading txt config file

--- src/main/java/com/example/tvserieapp/TVserieAppApplication.java
+++ src/main/java/com/example/tvserieapp/TVserieAppApplication.java
@@ -8,72 +8,36 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.*;
+import java.sql.*;
 import java.util.List;
+import java.util.Properties;
 
 @SpringBootApplication
 @RestController
 public class TVserieAppApplication {
-    /*
-    public static void createNewTable(){
-        String url = "jdbc:sqlite:C://tvserier.db";
-
-        String sql = "CREATE TABLE IF NOT EXISTS tvserier (\n" +
-                "id integer PRIMARY KEY AUTOINCREMENT,\n" +
-                "url text,\n" +
-                "name text,\n" +
-                "type text,\n" +
-                "language text,\n" +
-                "genres text,\n" +
-                "status text,\n" +
-                "runtime text,\n" +
-                "averageRuntime text,\n" +
-                "premiered text,\n" +
-                "ended text,\n" +
-                "officialSite text,\n" +
-                "schedule text,\n" +
-                "rating text,\n" +
-                "weight text,\n" +
-                "network text,\n" +
-                "country text,\n" +
-                "webChannel text,\n" +
-                "dvdCountry text,\n" +
-                "externals text,\n" +
-                "image text,\n" +
-                "summary text,\n" +
-                "updated text,\n" +
-                "_links text,\n" +
-                "previousepisode text\n" +
-                ")";
-
-        try (Connection conn = DriverManager.getConnection(url);
-                Statement stmt = conn.createStatement()){
-            stmt.execute("");
-        }catch (SQLException e){
-            System.out.println(e.getMessage());
-        }
-    }*/
-
     public static void main(String[] args) {
         SpringApplication.run(TVserieAppApplication.class, args);
+
+        File file = new File("src/main/resources/tvserier.txt");
+        try {
+            FileInputStream innSerier = new FileInputStream(file);
+            BufferedReader utSerier = new BufferedReader(new InputStreamReader(innSerier));
+            String data;
+            while ((data = utSerier.readLine()) != null){
+                String[] shows = data.split("\\\\n"); // split each line from .txt file
+                for (String show : shows){
+                    System.out.println(show); // print each show in .txt file as test
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @GetMapping("/hello")
     public String sayHello(@RequestParam(value = "myName", defaultValue = "World") String name) {
         return String.format("Hello %s!", name);
     }
-
-    /*@Override
-    public void run(String... args) throws Exception {
-        jdbcTemplate.execute("CREATE TABLE IF NOT EXISTS tvserier(name VARCHAR(100))");
-
-        jdbcTemplate.execute("INSERT INTO tvserier VALUES ('Breaking Bad')");
-
-        List<Show> shows = jdbcTemplate.query("SELECT * FROM tvserier",
-        (resultSet, rowNum) -> new Show(resultSet.getString("name")));
-    }*/
 
 }
